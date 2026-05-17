80e366848455cd83ae347f6bd5471058c42c288c
milad <m.moltaji@yahoo.com>
2026-05-17 10:07:36 +0330

    add filter somare pos in resid

--- DataAnalysis/views.py
+++ DataAnalysis/views.py
@@ -517,6 +517,7 @@ def sepidar_download_excel(request):
 
     CODE_MOIN_POS_MOTOFAREGHE = 121304
     MOSTARAK_FANTEZI = 20142
+    POS_FANTEZI = 11
  
     codes = (Profile.objects
             .exclude(code_vaset__isnull=True)
@@ -579,10 +580,12 @@ def sepidar_download_excel(request):
                 int(inv.moshtarak)
             except:
                 inv.moshtarak = 0
-            if int(inv.moshtarak) ==1:
+            if int(inv.moshtarak) == 1:
                 moshtarak = 10059  ### SNAPP DEFAULT CODE
             elif 10000<int(inv.moshtarak)<=10140:
                 moshtarak = inv.moshtarak
+            elif int(inv.shomare_pos) == POS_FANTEZI :
+                moshtarak = MOSTARAK_FANTEZI
             else:
                 moshtarak = MOSHTARAK_DEFAULT_CODE
 
